3af06e7896cc5caf6c59b65432322cad0aca5984
Mina <41920626+Minayaff@users.noreply.github.com>
2022-01-15 23:06:10 +0400

    Add files via upload

---
+++ Lesson-7/Csharp05/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Ad daxil et");
+            //string name = Console.ReadLine();
+
+            //Console.WriteLine("Soyad daxil et");
+            //string surname = Console.ReadLine();
+
+            //StudentCsharp04 student = new StudentCsharp04(name, surname);
+            //StudentCsharp04 student = new StudentCsharp04("Mina", "Farzali");
+            //student.GetFullname();
+            //StudentCsharp04.Write();
+            //Console.WriteLine(student.Name);
+
+            int[] a = new int[5];
+            a[0] = 5;
+            a[1] = 6;
+            a[2] = 3;
+
+            //ARRAYLIST-tipi olmur=>boxing unboxinge cixir , olcusu olmur
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add(5);
+            arrayList.Add(5);
+            arrayList.Add(true);
+
+            StudentCsharp04 student = new StudentCsharp04("Mina", "F");
+
+            arrayList.Add(student);
+
+            //foreach (var item in arrayList)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //if (arrayList[0] is int)
+            //{
+            //    int a1 = (int)arrayList[1];
+            //    Console.WriteLine(a1);
+            //}
+            arrayList.Remove(5);
+            //arrayList.RemoveAt(2);
+
+            foreach (var item in arrayList)
+            {
+                Console.WriteLine(item);
+            }
+
+            arrayList.AddRange(a);
+        }
+
+
+    }
+
+
+}
---
+++ Lesson-7/Csharp05/StudentCsharp04.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp05
+{
+    class StudentCsharp04
+    {
+        public string Name;
+        public string Surname;
+
+        public StudentCsharp04(string nm,string srname)
+        {
+            Name = nm;
+            Surname = srname;
+        }
+
+        public static void Write()  //return 
+        {
+            Console.WriteLine("staticccc");
+        }
+
+        public void GetFullname() //return type yoxdur console.write
+        {
+            Console.WriteLine("staticcc olmayan");
+        }
+
+    }
+}
