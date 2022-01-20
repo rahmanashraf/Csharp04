e5d9409dfadd94aab4af7b00f09627dd898176da
Mina <41920626+Minayaff@users.noreply.github.com>
2022-01-20 23:59:35 +0400

    Array add method

---
+++ AyselTask/Csharp05/StudentCsharp04.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp05
+{
+    class StudentCsharp04
+    {
+        public string Name;
+        public string Surname;
+
+        public StudentCsharp04(string nm,string srname)
+        {
+            Name = nm;
+            Surname = srname;
+        }
+
+    }
+}
